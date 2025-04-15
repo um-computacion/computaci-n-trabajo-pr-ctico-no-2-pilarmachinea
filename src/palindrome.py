43cfebeba3aa37fe98824f5f55a729a88feccd24
pilarmachinea <m.machinea@alumno.um.edu.ar>
2025-04-14 22:05:41 -0300

    comparacion de caracteres

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,12 +1,16 @@
 import re
 
-def is_palindrome(text: str) -> bool:
+def is_palindrome_basic(text: str) -> bool:
     return text == text[::-1]
 
 def clean_text(text: str) -> str:
     text = text.lower().strip()
     # Eliminar caracteres no alfanuméricos
     return re.sub(r'[^a-z0-9]', '', text)
+
+def is_palindrome(text: str) -> bool:
+    cleaned = clean_text(text)
+    return is_palindrome_basic(cleaned)
 
 if __name__ == '__main__':
     frase = input("Ingrese una palabra o frase: ")
